fd1c875cda8c8f847101d5b1d004815e923c2713
Leslie Moses <LesMoses@gmail.com>
2017-05-03 12:52:55 -0400

    copy past'g

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,4 @@
+var express = require("express");
+
+// Import the model (cat.js) to use its database functions.
+var cat = require("../models/burger.js");
---
+++ models/burger.js
@@ -0,0 +1,6 @@
+var orm = require("../config/orm.js");
+
+
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
