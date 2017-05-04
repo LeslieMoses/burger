57bc47c7da12e3d12ab8822d5ccb2641f36f814f
Leslie Moses <LesMoses@gmail.com>
2017-05-04 00:37:22 -0400

    deploying

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,28 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+var mysql = require("mysql");;
+var connection;
 
-var connection = mysql.createConnection({
-    port: 3305,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
 
-// Make connection.
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-// Export connection for our ORM to use.
-module.exports = connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({}
+            var connection = mysql.createConnection({
+                // keep port in? instructions d mention .
+                port: 3305,
+                host: "localhost",
+                user: "root",
+                password: "root",
+                database: "burgers_db"
+            });
+
+            // Make connection.
+            connection.connect(function(err) {
+                if (err) {
+                    console.error("error connecting: " + err.stack);
+                    return;
+                }
+                console.log("connected as id " + connection.threadId);
+            });
+            // Export connection for our ORM to use.
+            module.exports = connection;
