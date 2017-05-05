46618537c18fca45eec8bb92332a2c26a5aa3298
Leslie Moses <LesMoses@gmail.com>
2017-05-05 19:51:53 -0400

    whyyyyyy

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,12 +2,12 @@ var express = require("express");
 //copied from catsController.js 
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
+// Import the model (burger.js) 2 use its database fn.s
 var burger = require("../models/burger.js");
 
-
 // CC de catsController.js
-// Create all our routes and set up logic within those routes where required.
+// Create all our routes + set up logic w/n 
+// those routes where required.
 router.get("/", function(req, res) {
     burger.all(function(data) {
         var hbsObject = {
